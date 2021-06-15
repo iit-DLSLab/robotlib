4ea8579708771b45a633b600934d5c48ecda9bea
Gianluca <gianluca.cerilli@iit.it>
2021-06-15 18:24:12 +0200

    Added some temporary parameters

--- include/forward_kinematics_base.hpp
+++ include/forward_kinematics_base.hpp
@@ -7,13 +7,23 @@
 namespace dls{
 namespace robot {
 
+/// --> TODO: Remove
+static const int fbjointsCount = 18;
+typedef Eigen::Matrix<double, fbjointsCount, 1> Column18d;
+typedef Column18d FloatingBaseJointState;
+
+typedef Eigen::Matrix<double, 12, 1> Column12d;
+typedef Column12d JointState;
+typedef Eigen::Array<bool, 3, 1> LegJointBool;
+/// <--
+
 class ForwardKinematicsBase
 {
     public:
         virtual ~ForwardKinematicsBase(){}
 
         // TODO: Consider the possibility of using an enum for the LegID
-        virtual Eigen::Vector3d getFootPos(const JointState& q, const int LegID& leg) = 0;
+        virtual Eigen::Vector3d getFootPos(const JointState& q, const int leg) = 0;
 };
 
 }
