3aadb8bb64f829f184500975bbe95133f0b63591
Gianluca <gianluca.cerilli@iit.it>
2021-06-16 12:07:59 +0200

    Removed some typedef

--- include/forward_kinematics_base.hpp
+++ include/forward_kinematics_base.hpp
@@ -3,24 +3,21 @@
 
 #include "leg_data_map.hpp"
 #include <Eigen/Dense>
+#include <iostream>
 
 namespace dls{
 namespace robot {
 
 /// --> TODO: Remove
-static const int fbjointsCount = 18;
-typedef Eigen::Matrix<double, fbjointsCount, 1> Column18d;
-typedef Column18d FloatingBaseJointState;
-
 typedef Eigen::Matrix<double, 12, 1> Column12d;
 typedef Column12d JointState;
-typedef Eigen::Array<bool, 3, 1> LegJointBool;
 /// <--
 
 class ForwardKinematicsBase
 {
     public:
-        virtual ~ForwardKinematicsBase(){}
+        ForwardKinematicsBase(){};
+        virtual ~ForwardKinematicsBase(){};
 
         // TODO: Consider the possibility of using an enum for the LegID
         virtual Eigen::Vector3d getFootPos(const JointState& q, const int leg) = 0;
