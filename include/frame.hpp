5fd8079c80822ff0e6306881b0b0aef312124ca9
Gianluca <gianluca.cerilli@iit.it>
2021-06-28 12:03:43 +0200

    frame, joint and link classes

--- include/frame.hpp
+++ include/frame.hpp
@@ -1,6 +1,9 @@
 #ifndef _ROBOTLIB_FRAME_HPP_
 #define _ROBOTLIB_FRAME_HPP_
 
+#include "limb_base.hpp"
+#include <string>
+
 namespace dls
 {
     namespace robotlib
@@ -11,7 +14,13 @@ namespace dls
             Frame(){};
             virtual ~Frame(){};
 
-            virtual bool isIdentifier(std::string &frame_name) = 0;
+            virtual const std::string getName() = 0;
+            virtual const LimbBase *getParent() const = 0;
+
+        protected:
+            std::string name_{};
+            const LimbBase *parent_{};
+            //const DynParams dyn_params_;	        //! Dynamic parameter of the joint
         };
     } // namespace robotlib
 } // namespace dls
--- include/link.hpp
+++ include/link.hpp
@@ -1,36 +1,21 @@
 #ifndef _ROBOTLIB_LINK_HPP_
 #define _ROBOTLIB_LINK_HPP_
 
-#include "limb_base.hpp"
+#include "frame.hpp"
 
 namespace dls
 {
 	namespace robotlib
 	{
-		/**
- 		* A link class for robots.
- 		*/
-		class Link
+		class Link : public Frame
 		{
 		public:
-			Link(LimbBase *parent, const std::string &name) : parent_(parent), name_(name){};
+			Link(LimbBase *parent, const std::string &name);
 
-			//Link (const std::string& name, const Pose& pose, const DynParams& dparams ): name_(name), pose_(pose), dyn_params_(dparams) {};
+			virtual ~Link();
 
-			~Link(){};
-
-			// Get functions
-			const std::string getName() const { return name_; }
-			const LimbBase *getParent() const { return parent_; }
-
-			//const Pose& getPose(){return pose_;}
-			//const DynParams& getDynParams(){return dyn_params_;}
-
-		private:
-			const std::string name_; //! Name of the link
-			const LimbBase *parent_; //! Pointer to parent limb
-									 // const Pose pose_;		                //! Pose of the link w.r.t. parent link
-									 // const DynParams dyn_params_; 		    //! Dynamic parameter of the link
+			virtual const std::string getName() override;
+			virtual const LimbBase *getParent() const override;
 		};
 	} // namespace robotlib
 } // namespace dls
