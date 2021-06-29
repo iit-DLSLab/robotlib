d9b11972984140453beceb28f68ab5404cb2861b
Gianluca <gianluca.cerilli@iit.it>
2021-06-29 10:05:53 +0200

    Removed set methods from kyn_dyn_params

--- include/kin_dyn_params.hpp
+++ include/kin_dyn_params.hpp
@@ -25,33 +25,12 @@ namespace dls
             virtual double getValue_trunk_Ixz() const = 0;
             virtual double getValue_trunk_Iyz() const = 0;
 
-            virtual void setValue_robot_total_mass(double val) = 0;
-            virtual void setValue_trunk_mass(double val) = 0;
-
-            virtual void setValue_trunk_com_x(double val) = 0;
-            virtual void setValue_trunk_com_y(double val) = 0;
-            virtual void setValue_trunk_com_z(double val) = 0;
-
-            virtual void setValue_trunk_Ix(double val) = 0;
-            virtual void setValue_trunk_Iy(double val) = 0;
-            virtual void setValue_trunk_Iz(double val) = 0;
-            virtual void setValue_trunk_Ixy(double val) = 0;
-            virtual void setValue_trunk_Ixz(double val) = 0;
-            virtual void setValue_trunk_Iyz(double val) = 0;
-
             virtual double getValue_foot_x() const = 0;
-
-            virtual void setValue_foot_x(double val) = 0;
 
             virtual double getValue_LF_shin() const = 0;
             virtual double getValue_RF_shin() const = 0;
             virtual double getValue_LH_shin() const = 0;
             virtual double getValue_RH_shin() const = 0;
-
-            virtual void setValue_LF_shin(double val) = 0;
-            virtual void setValue_RF_shin(double val) = 0;
-            virtual void setValue_LH_shin(double val) = 0;
-            virtual void setValue_RH_shin(double val) = 0;
 
             virtual double getValue_haa_x() const = 0;
             virtual double getValue_haa_y() const = 0;
@@ -60,12 +39,6 @@ namespace dls
             virtual double getValue_haa_hfe() const = 0;
             virtual double getValue_upper_leg() const = 0;
             virtual double getValue_lower_leg() const = 0;
-
-            virtual void setValue_haa_x(double val) = 0;
-            virtual void setValue_haa_y(double val) = 0;
-            virtual void setValue_haa_hfe(double val) = 0;
-            virtual void setValue_upper_leg(double val) = 0;
-            virtual void setValue_lower_leg(double val) = 0;
         };
 
     } // namespace robotlib
