3390fc9c8fe6cddd4c51444b09d43c7f5293ffdf
Gianluca <gianluca.cerilli@iit.it>
2021-07-07 15:20:29 +0200

    fixing hardcoded paths in CMakeLists files

---
+++ cmake/robotlib_options.cmake
@@ -0,0 +1,15 @@
+set(ROBOTLIB_INSTALL_HEADERS_DIR /usr/include/robotlib
+    CACHE STRING "Directory where the robotlib headers are stored"
+)
+
+set(ROBOTLIB_INSTALL_RUNTIME_DIR /usr/bin/robotlib
+	CACHE STRING "Directory where the robotlib executables are stored"
+)
+
+set(ROBOTLIB_INSTALL_LIBRARIES_DIR /usr/lib/robotlib
+	CACHE STRING "Directory where the robotlib shared object libraries are stored"
+)
+
+set(EIGEN_DIR /usr/include/eigen3
+	CACHE STRING "Directory where Eigen is installed"
+)
