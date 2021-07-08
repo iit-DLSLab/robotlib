f6e69942657230ed75c432a33f3b97efd050f1e5
Gianluca <gianluca.cerilli@iit.it>
2021-07-08 14:47:08 +0200

    fixed error in try-catch library path handling

--- src/robot_factory.cpp
+++ src/robot_factory.cpp
@@ -7,43 +7,67 @@ namespace dls
 		std::shared_ptr<RobotBase> RobotFactory::openRobot(const std::string &robot_type)
 		{
 			std::string library{"lib" + robot_type + ".so"};
-			void *robot{};
+			void *robot{nullptr};
 
-			// Look for the robot library in the current directory and load it
-			try
-			{
-				std::string lib_path{"./" + library};
-				robot = dlopen(lib_path.c_str(), RTLD_LAZY);
-				if (!robot)
-					throw dlerror();
-			}
-			catch (const std::exception &e)
-			{
-				std::cout << e.what();
-			}
-
-			// Look for the robot library in the robots directory and load it
+			/// Look for the robot library in the robots directory and load it
 			try
 			{
 				std::string lib_path{"/usr/lib/robots/robotlib/" + library};
+
+				std::cout << "Look for " << lib_path << " library" << std::endl;
+
 				robot = dlopen(lib_path.c_str(), RTLD_LAZY);
-				if (!robot)
-					throw dlerror();
+
+				if (robot)
+				{
+					std::cout << "Found " << lib_path << std::endl;
+
+					/// Load the create symbol
+					RobotBase::createRobot_t *create_robot = (RobotBase::createRobot_t *)dlsym(robot, "createRobot_t");
+					return create_robot();
+				}
+				else
+				{
+					robot = nullptr;
+					const std::string error{dlerror()};
+					throw error;
+				}
 			}
-			catch (const std::exception &e)
+			catch (const std::string &e)
 			{
-				std::cout << e.what();
+				std::cout << e << std::endl;
 			}
 
-			// Load the create symbol
-			RobotBase::createRobot_t *create_robot = (RobotBase::createRobot_t *)dlsym(robot, "createRobot_t");
-			const char *dlsym_error = dlerror();
-			if (dlsym_error)
+			/// Look for the robot library in the current directory and load it
+			try
 			{
-				std::cerr << "Cannot load symbol create: " << dlsym_error << '\n';
+				std::string lib_path{"./" + library};
+
+				std::cout << "Look for " << lib_path << " library" << std::endl;
+
+				robot = dlopen(lib_path.c_str(), RTLD_LAZY);
+
+				if (robot)
+				{
+					std::cout << "Found " << lib_path << std::endl;
+
+					/// Load the create symbol
+					RobotBase::createRobot_t *create_robot = (RobotBase::createRobot_t *)dlsym(robot, "createRobot_t");
+					return create_robot();
+				}
+				else
+				{
+					robot = nullptr;
+					const std::string error{dlerror()};
+					throw error;
+				}
+			}
+			catch (const std::string &e)
+			{
+				std::cout << e << std::endl;
 			}
 
-			return create_robot();
+			exit(EXIT_FAILURE);
 		}
 	} // namespace robotlib
 } // namespace dls
