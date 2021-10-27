098bc63a8f5d3a2251bdf27565fcf2f822cfbb36
Gianluca <gianluca.cerilli@iit.it>
2021-10-27 10:57:48 +0200

    joint set methods as private again

--- include/joint.hpp
+++ include/joint.hpp
@@ -54,7 +54,7 @@ namespace dls
 			 */
 			const std::shared_ptr<Link> getChild() const;
 
-		protected:
+		private:
 			/**
 			 * @brief Set the Joint parent object, that is a Link object
 			 * @param parent the Joint parent object to be set
@@ -68,6 +68,7 @@ namespace dls
 			 */
 			void setChild(const std::shared_ptr<Link> child);
 
+		protected:
 			std::shared_ptr<Link> parent_;
 			std::shared_ptr<Link> child_;
 		};
