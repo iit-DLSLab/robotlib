3da48b98b60c494d8153d3deb34e1ea13e71aecc
Gianluca <gianluca.cerilli@iit.it>
2021-11-03 16:58:25 +0100

    frame class documentation

--- include/frame.hpp
+++ include/frame.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file frame.hpp
+ *
+ * @brief Frame class definition and functions prototypes
+ *
+ * @author Gianluca Cerilli (IIT DLS Lab) - Contact: gianluca.cerilli@iit.it
+ * @author Marco Marchitto (IIT DLS Lab) - Contact: marco.marchitto@iit.it
+ *
+ * @bug No known bugs.
+ */
+
 #ifndef _ROBOTLIB_FRAME_HPP_
 #define _ROBOTLIB_FRAME_HPP_
 
@@ -7,6 +18,11 @@ namespace dls
 {
     namespace robotlib
     {
+        /**
+		* @brief Frame class
+		* @details
+		* This class represents a generic robot frame (joint/link) and is extended by Joint and Link classes
+		*/
         class Frame
         {
         public:
@@ -14,6 +30,10 @@ namespace dls
             //Frame(const Eigen::Vector3d &p, const Eigen::Vector3d &ori)
             virtual ~Frame(){};
 
+            /**
+			 * @brief Get the name of the Frame
+			 * @return const std::string
+			 */
             virtual const std::string getName() const = 0;
 
         protected:
