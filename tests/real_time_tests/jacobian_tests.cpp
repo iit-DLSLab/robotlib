f322d9694f1f16d315483504d5228b6daef2e375
mmarchitto <marco.marchitto@iit.it>
2022-06-22 18:19:48 +0200

    checking dynamic memory allocation after deactivating the hooks
    If you use is_dynamic_memory_used(...) between activate_hooks() and deactivate_hooks() you may intercept allocation/deallocation/reallocation due to is_dynamic_memory_used(...) function.
    It is still not clear why this happens, because is_dynamic_memory_used(...) is not using dynamic memory. But it is correct to add between activate_hooks() and deactivate_hooks() and the code you want to test.

--- tests/real_time_tests/jacobian_tests.cpp
+++ tests/real_time_tests/jacobian_tests.cpp
@@ -6,27 +6,33 @@
 #include <gtest/gtest.h>
 #include "dynamic_memory_hooks.hpp"
 
-TEST(RealTimeTest, eigen_multiplication)
+TEST(RealTimeTest, eigen_multiplication_dynamic_matrices)
 {
     Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic>A,B,C;
     A.setOnes(500,500);
     B.setOnes(500,500);
     C.setOnes(500,500);
 
+    // Dynamic memory allocation/deallocation have to take place
     reset_variables_checking_use_of_dynamic_memory();
     activate_hooks();
-   
-    // Eigen::internal::set_is_malloc_allowed(false);
-    // C = A*B;
-    // C.noalias() = A * B;
-    (C.noalias()=A.lazyProduct(B));
-    // Eigen::internal::set_is_malloc_allowed(true);
-    
-    is_dynamic_memory_used(false, false, false, false);
-    
+    C = A*B;
     deactivate_hooks();
+    is_dynamic_memory_used(true, false, false, true); // malloc, calloc, realloc, free
 
-      
+    // Dynamic memory allocation/deallocation have to take place
+    reset_variables_checking_use_of_dynamic_memory();
+    activate_hooks();
+    C.noalias() = A * B;
+    deactivate_hooks();
+    is_dynamic_memory_used(true, false, false, true); // malloc, calloc, realloc, free
+
+    // Dynamic memory allocation/deallocation have not to take place
+    reset_variables_checking_use_of_dynamic_memory();
+    activate_hooks();
+    (C.noalias()=A.lazyProduct(B));
+    deactivate_hooks();
+    is_dynamic_memory_used(false, false, false, false); // malloc, calloc, realloc, free
 }
 
 int main(int argc, char **argv)
