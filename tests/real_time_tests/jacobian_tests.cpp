82da5f533303b86975b594d769c5515b7626c12a
mmarchitto <marco.marchitto@iit.it>
2022-06-23 17:22:35 +0200

    added real-time test for eigen map multiplications
    Multiplication between Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> variables still lead to dynamic memory allocation when using e.g. C = A*B. The second check on dynamic memory usage is showing no malloc usage but the free function is used. It is needed to understand why this happens

--- tests/real_time_tests/jacobian_tests.cpp
+++ tests/real_time_tests/jacobian_tests.cpp
@@ -60,6 +60,52 @@ TEST(RealTimeTest, eigen_multiplication_static_matrices)
     is_dynamic_memory_used(false, false, false, false); // malloc, calloc, realloc, free
 }
 
+TEST(RealTimeTest, eigen_multiplication_map_matrices)
+{
+    // ** Define vectors to be mapped **
+    const int n_rows {25};
+    const int n_cols {25};
+    const int n_data {n_rows*n_cols};
+    double* data_A = new double[n_data];
+    double* data_B = new double[n_data];
+    double* data_C = new double[n_data];
+
+    for (int i=0; i< n_data; i++)
+    {
+        data_A[i] = 1.0;
+        data_B[i] = 1.0;
+        data_C[i] = 1.0;
+    }
+
+    // ** Define eigen map structures **
+    Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> A {data_A, n_rows, n_cols};
+    Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> B {data_B, n_rows, n_cols};
+    Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> C {data_C, n_rows, n_cols};
+
+    // ** Multiply eigen map structures **
+    // Dynamic memory allocation/deallocation have not to take place
+    reset_variables_checking_use_of_dynamic_memory();
+    activate_hooks();
+    C = A*B;
+    deactivate_hooks();
+    is_dynamic_memory_used(true, false, false, true); // malloc, calloc, realloc, free
+
+    // Dynamic memory allocation/deallocation have not to take place
+    reset_variables_checking_use_of_dynamic_memory();
+    activate_hooks();
+    C.noalias() = A * B;
+    deactivate_hooks(); 
+    is_dynamic_memory_used(false, false, false, true); // malloc, calloc, realloc, free 
+
+    // Dynamic memory allocation/deallocation have not to take place
+    reset_variables_checking_use_of_dynamic_memory();
+    activate_hooks();
+    (C.noalias()=A.lazyProduct(B));
+    deactivate_hooks();
+    is_dynamic_memory_used(false, false, false, false); // malloc, calloc, realloc, free
+    
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
