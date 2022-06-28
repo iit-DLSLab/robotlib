cbf822f5ae184a845dac99a97ac24d909f3bd601
mmarchitto <marco.marchitto@iit.it>
2022-06-28 18:05:47 +0200

    added real-time test on get functions for Jacobian class

--- tests/real_time_tests/jacobian_tests.cpp
+++ tests/real_time_tests/jacobian_tests.cpp
@@ -1,4 +1,5 @@
 #include "robot_base.hpp"
+#include "robot_factory.hpp"
 #include "dynamic_memory_hooks.hpp"
 #include <gtest/gtest.h>
 
@@ -167,7 +168,26 @@ TEST(RealTimeTest, eigen_multiplication_map_matrices)
     (C.noalias()=A.lazyProduct(B));
     deactivate_hooks();
     is_dynamic_memory_used(false, false, false, false); // malloc, calloc, realloc, free
+}
+
+TEST(RealTimeTest, get_functions)
+{
+    // ** Define robot **
+    std::shared_ptr<robotlib::RobotBase> robot {robotlib::RobotFactory::openRobot("dummy-quadruped")};
     
+    // ** Define feet jacobian **
+    robotlib::RobotBase::LegDataMap<robotlib::RobotBase::Jacobian> feet_jacobians {robot->makeFeetJacobian()};
+    
+    // Dynamic memory allocation/deallocation have not to take place
+    reset_variables_checking_use_of_dynamic_memory();
+    activate_hooks();
+    for(auto leg : *robot->getLegs())
+    {
+        Eigen::Map linear_jacobian {feet_jacobians[leg].getLinearJacobian()};
+        Eigen::Map angular_jacobian {feet_jacobians[leg].getAngularJacobian()};
+    }
+    deactivate_hooks();
+    is_dynamic_memory_used(false, false, false, false); // malloc, calloc, realloc, free
 }
 
 int main(int argc, char **argv)
