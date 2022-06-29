60e32e44e0220bf4423251c32e7a93d78a0db8d8
Douglas Bertol <douglas.bertol@iit.it>
2022-06-29 18:02:06 +0200

    upd: corrected deb pakaging

---
+++ cmake/debian_settings.cmake
@@ -0,0 +1,22 @@
+message(STATUS "## Reading debian_settings.cmake")
+
+set(CPACK_PACKAGE_VERSION_MAJOR         ${PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR         ${PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH         ${PROJECT_VERSION_PATCH})
+set(CPACK_DEBIAN_PACKAGE_VERSION        ${PROJECT_VERSION})
+set(CPACK_PACKAGE_VENDOR                "dlslab")
+set(CPACK_PACKAGE_CONTACT               "dlslab")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER     "dlslab")
+set(CPACK_VERBATIM_VARIABLES            ON)
+set(CPACK_GENERATOR                     "DEB")
+set(CPACK_DEB_COMPONENT_INSTALL         ON)
+
+set(CPACK_DEBIAN_ROBOTLIB_PACKAGE_NAME       "dls-robotlib" CACHE INTERNAL "")
+set(CPACK_DEBIAN_ROBOTLIB_FILE_NAME          "dls-robotlib-${PROJECT_VERSION}.deb" CACHE INTERNAL "")  
+
+set(CPACK_DEBIAN_ROBOTLIB_PACKAGE_DEPENDS    "dls2-runtime, dls2-messaging" CACHE INTERNAL "")
+
+get_cmake_property(CPACK_COMPONENTS_ALL COMPONENTS)
+list(REMOVE_ITEM CPACK_COMPONENTS_ALL "Unspecified")
+
+include(CPack)
