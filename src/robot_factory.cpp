087aac8eaa564602bb1a764c3cc82ed91df7ecfd
Douglas Bertol <dwbertol@gmail.com>
2022-10-31 22:25:04 +0100

    reallocate common libs to a new default installation folder

--- src/robot_factory.cpp
+++ src/robot_factory.cpp
@@ -8,9 +8,9 @@ namespace robotlib
 	{
 		std::string library{"lib" + robot_type + ".so"}, lib_path{};
 
-		if (std::filesystem::exists("/usr/lib/robots/" + library))
+		if (std::filesystem::exists("/usr/lib/" + library))
 		{
-			lib_path = "/usr/lib/robots/" + library;
+			lib_path = "/usr/lib/" + library;
 		}
 
 		else if (std::filesystem::exists("./" + library))
@@ -52,9 +52,9 @@ namespace robotlib
 	{
 		std::string library{"lib" + robot_type + ".so"}, lib_path{};
 
-		if (std::filesystem::exists("/usr/lib/robots/" + library))
+		if (std::filesystem::exists("/usr/lib/" + library))
 		{
-			lib_path = "/usr/lib/robots/" + library;
+			lib_path = "/usr/lib/" + library;
 		}
 
 		else if (std::filesystem::exists("./" + library))
